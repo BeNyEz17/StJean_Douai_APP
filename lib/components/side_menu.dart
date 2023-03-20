01aa0b3b6ab14d40b4153970f34bbc99018b83b4
Your Name <BeNyEz17>
2023-03-20 16:46:09 +0100

    Séparation des class

--- lib/components/side_menu.dart
+++ lib/components/side_menu.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stjean_douai_app/main.dart';
+import 'package:stjean_douai_app/components/info_card.dart';
+
 
 class SideMenu extends StatefulWidget {
   const SideMenu({Key? key}) : super(key: key);
@@ -15,27 +17,21 @@ class _SideMenuState extends State<SideMenu> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-      width: 288,
-      height: double.infinity,
+          width: 288,
+          height: double.infinity,
           color: d_color,
-          child:SafeArea(
-          child: Column(
+          child: SafeArea(
+              child: Column(
             children: [
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                      CupertinoIcons.person,
-                    color: Colors.blue,
-                  ),
-                ),
-                title: Text("Benjamin Pluchart"),
-              )
+              InfoCard(
+                name: "Benjamin",
+                option: "developper",
+              ),
             ],
-          )
-          ),
+          )),
         ),
       ),
     );
   }
 }
+
